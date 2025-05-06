a13e76bc011592b9909b095dd526f0f18f1db3e0
LETUNGDUONG999 <tungduongcamranh@gmail.com>
2025-05-06 08:52:43 -0700

    other smart contract

--- DeployNFT/wrappers/DeployNFTContract.compile.ts
+++ DeployNFT/wrappers/DeployNFTContract.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tact',
-    target: 'contracts/deploy_n_f_t_contract.tact',
+    target: 'contracts/TransferNFT.tact',
     options: {
         debug: true,
     },
